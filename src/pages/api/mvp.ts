b0d6a859bec06a57c2d4d50573df9cc832e15d9e
laglag1994 <roqayah1994@gmail.com>
2023-06-22 21:51:51 +0300

    req

--- src/pages/api/mvp.ts
+++ src/pages/api/mvp.ts
@@ -1,20 +1,47 @@
 import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 
-const prisma = new PrismaClient();
 
 
-//all mvps
-export default async function handler(req, res) {
-    if (req.method === 'GET') {
-      try {
-        const allMvps = await prisma.mvp.findMany();
-  
-        res.status(200).json(allMvps);
-      } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch MVPs' });
-      }
-    } else {
-      res.status(405).json({ message: 'Method Not Allowed' });
+export default async function mvpHandler(req: NextApiRequest, res: NextApiResponse) {
+  const prisma = new PrismaClient();
+
+  if (req.method === 'GET') {
+    try {
+      const mvps = await prisma.mvp.findMany();
+
+      res.status(200).json(mvps);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch MVPs' });
     }
+
+
+  } else if (req.method === 'POST') {
+    try {
+      const newMvp = await prisma.mvp.create({ data: req.body });
+
+      res.status(201).json(newMvp);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to create MVP' });
+    }
+
+
+
+  } else if (req.method === 'PUT') {
+    try {
+      const updateMvp = await prisma.mvp.update({
+        data: req.body,
+        where: {
+          id: req.body.id
+        }
+      })
+      res.status(201).json(updateMvp);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to update MVP' });
+    }
+
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
+}
