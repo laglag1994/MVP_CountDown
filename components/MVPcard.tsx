7196e8b34c9e0d06867d62756ba663237a3ab5ac
laglag1994 <roqayah1994@gmail.com>
2023-06-25 12:30:49 +0300

    date issue

--- components/MVPcard.tsx
+++ components/MVPcard.tsx
@@ -1,135 +1,77 @@
-import { data } from "autoprefixer"
-import { useEffect, useState } from "react"
-import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from 'react-query'
-
-
+import { useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from 'react-query';
 
 export type MvpCard = {
-    name: string
-    img?: string
-    respawnTime: number
-    lastKillTime?: Date
-    isAlive: boolean
-}
+    name: string;
+    img?: string;
+    respawnTime: number;
+    lastKillTime?: Date;
+    isAlive: boolean;
+};
 
 interface MvpProps {
-    cards: MvpCard[]
+    cards: MvpCard[];
 }
 
-const updateMvpInfo = async ({ mvpId, lastKillTime, alive }: { mvpId: number, lastKillTime: Date, alive: boolean }) => {
+const updateMvpInfo = async ({ mvpId, lastKillTime, alive, respDelayTime }: { mvpId: number, lastKillTime: Date, alive: boolean, respDelayTime: number }) => {
     const updatedData = await fetch("http://localhost:3000/api/mvp", {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ id: mvpId, isAlive: alive, respawnTime: lastKillTime }),
+        body: JSON.stringify({ id: mvpId, isAlive: alive, respawnTime: respDelayTime, lastKillTime: lastKillTime }),
     });
     return updatedData.json();
-}
-
+};
 
 const MVPcard: React.FC<MvpProps> = ({ cards }) => {
+    const queryClient = useQueryClient();
 
-
-
-    const queryClient = useQueryClient()
-
-    const { isLoading, error, data, mutateAsync, } = useMutation(
-        "updateMvp", updateMvpInfo
+    const { isLoading, error, data, mutateAsync } = useMutation(
+        "updateMvp",
+        updateMvpInfo
     );
-
-    console.log(data, "data")
 
     const handleMvpUpdate = async (id: number) => {
         const currentMvp = cards[id];
         const currentTime = new Date();
-        // const respawnTime = new Date(currentTime.getTime() + 90 * 60000); // Add 90 minutes (90 minutes * 60 seconds * 1000 milliseconds)
-
+        const difference = currentMvp.respawnTime - (currentMvp.lastKillTime instanceof Date ? currentMvp.lastKillTime.getTime() : 0);
         if (currentMvp.isAlive) {
             const alive = false;
             const res = await mutateAsync({
                 mvpId: id,
                 alive,
                 lastKillTime: currentTime,
+                respDelayTime: difference,
             });
             queryClient.invalidateQueries(["mvplist"]);
-            // console.log(res, "res")
-
         }
     };
-
-
+    console.log (data)
 
     if (isLoading) return <div>Loading...</div>;
-
     if (error) return <div>An error has occurred</div>;
 
-
-
-
-    const [countdown, setCountdown] = useState<{ hours: number; mins: number; sec: number }>({
-        hours: 0,
-        mins: 0,
-        sec: 0,
-    });
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            countountdown();
-        }, 1000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-
-    const countountdown = () => {
-        cards.forEach((card, index) => {
-            if (!card.isAlive && card.lastKillTime) {
-                const lastKill = new Date(card.lastKillTime);
-                const respTime = new Date(lastKill.getTime() + card.respawnTime)
-                // const different = lastKill.getTime() - respTime.getTime();
-
-                const different = respTime.getTime() - new Date().getTime()
-
-                const hours = Math.floor(different / (1000 * 3600)) % 24;
-                const mins = Math.floor(different / (1000 * 60)) % 60;
-                const sec = Math.floor(different / 1000) % 60;
-
-                if (index === 0) {
-                    setCountdown({ hours, mins, sec });
-                    card.isAlive = true
-                }
-            }
-        });
-    };
-
-
-
     return (
+        <div className="flex justify-center items-center gap-10 flex-wrap">
+            {cards.map((card, index) => {
+                const timeDifference = card.lastKillTime instanceof Date ? (card.respawnTime - card.lastKillTime.getTime()) : 0;
 
-        <div className="flex justify-center items-center gap-10 flex-wrap ">
-            {
-                cards.map((card, index) => {
-                    // console.log(cards, "cards")
-                    // const shortTime = card.respawnTime ? new Date(card.respawnTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: "2-digit" }) : '';
-                    return (
-                        <div key={index} className="flex flex-col justify-center items-center border-2 bg-[#DCD7C9] border-[#A27B5C] w-[200px]">
-                            <span>{card.name}</span>
-                            <span>
-                                <img className="custom-height" src={card.img} alt="" height="100px" />
-                            </span>
-                            <span>{card.isAlive ? "alive" : "dead"}</span>
-                            <span>{countdown.hours}: {countdown.mins}: {countdown.sec}</span>
-                            <button onClick={() => handleMvpUpdate(index)} className="bg-red-700 w-full text-white py-1">killed</button>
-                            <button className="bg-[#A27B5C] w-full text-white py-1">edit</button>
-                        </div>
-                    );
-                })
-            }
+                return (
+                    <div key={index} className="flex flex-col justify-center items-center border-2 bg-[#DCD7C9] border-[#A27B5C] w-[200px]">
+                        <span>{card.name}</span>
+                        <span>
+                            <img className="custom-height" src={card.img} alt="" height="100px" />
+                        </span>
+                        <span>{card.isAlive ? "alive" : "dead"}</span>
+                        <span>{timeDifference}</span>
+                        <button onClick={() => handleMvpUpdate(index)} className="bg-red-700 w-full text-white py-1">killed</button>
+                        <button className="bg-[#A27B5C] w-full text-white py-1">edit</button>
+                    </div>
+                );
+            })}
         </div>
-    )
-}
+    );
+};
 
-
-export default MVPcard
+export default MVPcard;
